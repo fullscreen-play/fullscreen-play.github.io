8bf7e49a719dabc878852397b82ce016b5c8ef22
Marziyeh Ahmed <marziyeh.naaz.ahmed@gmail.com>
2016-02-10 12:26:29 +0000

    scrollTo

--- main.js
+++ main.js
@@ -1,42 +1,51 @@
-// window.scrollTo(0,1);
+// // window.scrollTo(0,1);
+//
+// console.log("yo");
+//
+// // Find the right method, call on correct element
+// function launchIntoFullscreen(element) {
+//   if(element.requestFullscreen) {
+//     element.requestFullscreen();
+//   } else if(element.mozRequestFullScreen) {
+//     element.mozRequestFullScreen();
+//   } else if(element.webkitRequestFullscreen) {
+//     element.webkitRequestFullscreen();
+//   } else if(element.msRequestFullscreen) {
+//     element.msRequestFullscreen();
+//   }
+// }
+//
+// // Launch fullscreen for browsers that support it!
+// launchIntoFullscreen(document.documentElement); // the whole page
+// (function(){
+//     function launchIntoFullscreen(element) {
+//         console.log("launchintofullscreen");
+//         if(element.requestFullscreen) {
+//             element.requestFullscreen();
+//         } else if(element.mozRequestFullScreen) {
+//             element.mozRequestFullScreen();
+//         } else if(element.webkitRequestFullscreen) {
+//             element.webkitRequestFullscreen();
+//         } else if(element.msRequestFullscreen) {
+//             element.msRequestFullscreen();
+//         }
+//     }
+//
+//     document.addEventListener("click", function(){
+//         //window full screen tonen
+//         launchIntoFullscreen(document.documentElement);
+//         //window niet meer focussen (zorgt dat melding niet meer op scherm komt wanneer er over 2de window wordt gehovered
+//         $(document).blur();
+//     }, true);
+// })();
+//
+// // $('window').scrollTop(1);
 
-console.log("yo");
-
-// Find the right method, call on correct element
-function launchIntoFullscreen(element) {
-  if(element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if(element.mozRequestFullScreen) {
-    element.mozRequestFullScreen();
-  } else if(element.webkitRequestFullscreen) {
-    element.webkitRequestFullscreen();
-  } else if(element.msRequestFullscreen) {
-    element.msRequestFullscreen();
-  }
-}
-
-// Launch fullscreen for browsers that support it!
-launchIntoFullscreen(document.documentElement); // the whole page
-(function(){
-    function launchIntoFullscreen(element) {
-        console.log("launchintofullscreen");
-        if(element.requestFullscreen) {
-            element.requestFullscreen();
-        } else if(element.mozRequestFullScreen) {
-            element.mozRequestFullScreen();
-        } else if(element.webkitRequestFullscreen) {
-            element.webkitRequestFullscreen();
-        } else if(element.msRequestFullscreen) {
-            element.msRequestFullscreen();
-        }
-    }
-
-    document.addEventListener("click", function(){
-        //window full screen tonen
-        launchIntoFullscreen(document.documentElement);
-        //window niet meer focussen (zorgt dat melding niet meer op scherm komt wanneer er over 2de window wordt gehovered
-        $(document).blur();
-    }, true);
-})();
-
-// $('window').scrollTop(1);
+// When ready...
+window.addEventListener("load",function() {
+	// Set a timeout...
+	setTimeout(function(){
+		// Hide the address bar!
+		window.scrollTo(0, 1);
+	}, 0);
+});
